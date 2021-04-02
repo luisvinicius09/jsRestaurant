557db4e1e8a47933f3933b861cf6dc373973b512
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-02 13:54:51 -0300

    Create buttons events to display different content

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,41 @@
-console.log('This file is working perfect!')
+import Header from '../js/header';
+import Home from '../js/homepage'
+import Menu from '../js/menu';
+import Contact from '../js/contact'
+
+const content = document.querySelector('#content');
+const mainContainer = document.createElement('div');
+
+mainContainer.classList.add('main-container');
+content.appendChild(mainContainer)
+
+Header();
+Home();
+Menu();
+Contact();
+
+const wrapper = document.querySelector('.wrapper');
+const menuWrapper = document.querySelector('.menuWrapper');
+const contactWrapper = document.querySelector('.contactWrapper');
+
+const homeBtn = document.querySelector('.homeBtn')
+homeBtn.addEventListener('click', () => {
+  wrapper.classList.remove('d-none');
+  menuWrapper.classList.add('d-none');
+  contactWrapper.classList.add('d-none');
+})
+
+const menuBtn = document.querySelector('.menuBtn')
+menuBtn.addEventListener('click', () => {
+  menuWrapper.classList.remove('d-none');
+  wrapper.classList.add('d-none');
+  contactWrapper.classList.add('d-none');
+})
+
+
+const contactBtn = document.querySelector('.contactBtn')
+contactBtn.addEventListener('click', () => {
+  contactWrapper.classList.remove('d-none')
+  wrapper.classList.add('d-none');
+  menuWrapper.classList.add('d-none');
+})
