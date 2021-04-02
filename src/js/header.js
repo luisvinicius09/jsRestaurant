6e3ed161348fe21a4eea1f38b14f526e8821a304
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-02 13:53:19 -0300

    Modularize the header contenct

---
+++ src/js/header.js
@@ -0,0 +1,31 @@
+import home from "./homepage";
+
+const header = () => {
+  const mainContainer = document.querySelector('.main-container')
+
+  const header = document.createElement('header');
+  const headerContainer = document.createElement('div');
+  const homeBtn = document.createElement('a');
+  homeBtn.classList.add('homeBtn')
+  homeBtn.href = '#';
+  homeBtn.innerText = 'Home'
+
+  const menuBtn = document.createElement('a');
+  menuBtn.classList.add('menuBtn')
+  menuBtn.href = '#';
+  menuBtn.innerHTML = 'Menu'
+
+  const contactBtn = document.createElement('a');
+  contactBtn.classList.add('contactBtn');
+  contactBtn.href = '#';
+  contactBtn.innerText = 'Contact'
+
+  const buttons = [homeBtn, menuBtn, contactBtn]
+  for(let i=0; i < buttons.length; i++) {
+    headerContainer.appendChild(buttons[i]);
+  }
+  header.appendChild(headerContainer);
+  mainContainer.appendChild(header);
+}
+
+export default header;
