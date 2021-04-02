8c67ce10feed7b778c4af4ee89acf0ac282300af
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-02 19:37:08 -0300

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    open: true,
+    openPage: '/dist'
+  },
   mode: 'development',
 };
